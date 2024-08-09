7896453d0a4ccc753c6b5087e09358fb88e332df
Janit Yadav <janityadav16@gmail.com>
2024-08-09 21:58:21 +0530

    preparation for new app

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Weather from './component/weather/weather';
+import SearchBar from './component/search/searchmenu';
 
-function App() {
+const App: React.FC = () => {
+  const [city, setCity] = useState('');
+
+  const handleSearch = (city: string) => {
+    setCity(city);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      <Weather city={city} />
     </div>
   );
-}
+};
 
 export default App;
